7d949483394519a3607a16c3ae0d8e32424121de
Ndonna <ndonnauc@gmail.com>
2020-05-25 11:01:51 +0100

    Faulty Toggle Bar

---
+++ resources/script.js
@@ -0,0 +1,14 @@
+const tabs = document.querySelectorAll('.tablinks');
+const tabContents = document.querySelectorAll('.tab');
+
+tabs.forEach((tab, index) => tab.addEventListener('click', () => {
+  //reset non active class state everytime a tab is clicked
+  tabs.forEach((tab, index) => {
+    tab.classList.remove('active');
+    tabContents[index].classList.remove('show');
+  });
+
+  //now add active class to currently clicked tab
+  tab.classList.add('active');
+  tabContents[index].classList.add('show');
+}) );
